969b1c2014838331724c7ba8a37c74e5a84c6e5c
Anibal Velarde <anibalvelarde@gmail.com>
2016-03-09 21:32:50 -0600

    Added generic 'not implemented' pages.

---
+++ src/AppEstimator.Mvc/Controllers/NotImplementedController.cs
@@ -0,0 +1,21 @@
+﻿using AppEstimator.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppEstimator.Mvc.Controllers
+{
+    public class NotImplementedController : Controller
+    {
+        // GET: NotReadyMessage
+        public ActionResult Index(string message)
+        {
+            if (string.IsNullOrEmpty(message)) { message = "Action has not been implemented yet."; }
+            var redirectUrl = Request.UrlReferrer.ToString();
+            var niVm = new NotImplementedViewModel(message, redirectUrl);
+            return View(niVm);
+        }
+    }
+}
---
+++ src/AppEstimator.Mvc/Models/NotImplementedViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppEstimator.Mvc.Models
+{
+    public class NotImplementedViewModel
+    {
+        public NotImplementedViewModel(string message, string redirectUrl)
+        {
+            this.Message = message;
+            this.RedirectUrl = redirectUrl;
+        }
+
+        public string Message { get; private set; }
+        public string RedirectUrl { get; private set; }
+    }
+}
